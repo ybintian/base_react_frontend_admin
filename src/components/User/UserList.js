5251b6eca6003701508f522731c1272205d062c6
will <will@agideo.com>
2018-03-26 21:07:04 +0800

    feat 简单翻译

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'dva';
 import { Table } from 'antd';
+import zh_CN from '../../locales/zh_CN';
 
 function mapStateToProps(state) {
   return {
@@ -20,17 +21,24 @@ export class UserList extends Component {
 
   render() {
     const columns = [
-    { title: 'id', dataIndex: 'id', key: 'id'}, 
-    { title: 'username', dataIndex: 'username', key: 'username'},
-    { title: 'email', dataIndex: 'email', key: 'email'},
-    { title: 'Action', key: 'action', render: (text, record) => (
+    { dataIndex: 'id', key: 'id'}, 
+    { dataIndex: 'username', key: 'username'},
+    { dataIndex: 'email', key: 'email'},
+    { title: 'action', key: 'action', render: (text, record) => (
         <span>
           <a style={{margin: 5}}>详情</a>
           <a style={{margin: 5}}>编辑</a>
           <a style={{margin: 5}}>删除</a>
         </span>
       ),
-    }];
+    }].map(res => {
+      if (!res.title){
+        return {...res, title: zh_CN.user[res.dataIndex] || res.dataIndex};  
+      } else {
+        return res;
+      }
+      
+    });
 
     return(
       <Table rowKey="id" columns={columns} dataSource={this.props.results} />
